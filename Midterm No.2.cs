b34155689d3b8b522375065868e8eb95daf88a37
Upsornsri Ketchum <78495462+Canvas10678@users.noreply.github.com>
2021-03-28 18:11:19 +0700

    Add files via upload

---
+++ Midterm No.2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Midterm_No._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Input your Number a : ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Input your Number b :");
+            int b = int.Parse(Console.ReadLine());
+            int x = a;
+            int y = b;
+
+            do
+            {
+                if (x < y)
+                {
+                    x = x + a;
+
+                }
+                else
+                {
+                    y = y + b;
+                }
+
+            }
+            while (x != y);
+
+            Console.WriteLine("Output of X is {0}", x);
+        }
+    }
+}
